e9f8b46805091dd8834a9ba7590d3381f5758671
Daniel Eriksson <eriksson1977@gmail.com>
2008-08-23 19:16:12 +0200

    Month names and day names should not be capitalized.

--- lib/sv/date_time.rb
+++ lib/sv/date_time.rb
@@ -1,21 +1,32 @@
 module SwedishRails
-  REPLACEMENTS = { 
-    'January' => 'Januari', 'February' => 'Februari', 'March' => 'Mars',
-    'May' => 'Maj', 'June' => 'Juni', 'July' => 'Juli',
-    'August' => 'Augusti', 'October' => 'Oktober',
-    'Sunday' => 'Söndag', 'Monday' => 'Måndag', 'Tuesday' => 'Tisdag',
-    'Wednesday' => 'Onsdag', 'Thursday' => 'Torsdag', 'Friday' => 'Fredag',
-    'Saturday' => 'Lördag', 'May' => 'Maj', 'Oct' => 'Okt',
-    'Sun' => 'Sön', 'Mon' => 'Mån', 'Tue' => 'Tis', 'Wed' => 'Ons',
-    'Thu' => 'Tors', 'Fri' => 'Fre', 'Sat' => 'Lör',
+  REPLACEMENTS = {
+    'January' => 'januari', 'February' => 'februari', 'March' => 'mars', 'May' => 'maj',
+    'June' => 'juni', 'July' => 'juli', 'August' => 'augusti', 'September' => 'september', 
+    'October' => 'oktober', 'November' => 'november', 'December' => 'december',
+
     'january' => 'januari', 'february' => 'februari', 'march' => 'mars',
     'may' => 'maj', 'june' => 'juni', 'july' => 'juli',
     'august' => 'augusti', 'october' => 'oktober',
+
+    'Jan' => 'jan', 'Feb' => 'feb', 'Mar' => 'mar', 'Apr' => 'apr', 'May' => 'maj', 
+    'Jun' => 'jun', 'Jul' => 'jul', 'Aug' => 'aug', 'Sep' => 'sep', 'Oct' => 'okt', 
+    'Nov' => 'nov', 'Dec' => 'dec',
+
+    'may' => 'maj', 'oct' => 'okt',
+
+    'Sunday' => 'söndag', 'Monday' => 'måndag', 'Tuesday' => 'tisdag',
+    'Wednesday' => 'onsdag', 'Thursday' => 'torsdag', 'Friday' => 'fredag',
+    'Saturday' => 'lördag', 
+            
     'sunday' => 'söndag', 'monday' => 'måndag', 'tuesday' => 'tisdag',
     'wednesday' => 'onsdag', 'thursday' => 'torsdag', 'friday' => 'fredag',
-    'saturday' => 'lördag', 'may' => 'maj', 'oct' => 'okt',
+    'saturday' => 'lördag', 
+
+    'Sun' => 'sön', 'Mon' => 'mån', 'Tue' => 'tis', 'Wed' => 'ons',
+    'Thu' => 'tors', 'Fri' => 'fre', 'Sat' => 'lör',
+        
     'sun' => 'sön', 'mon' => 'mån', 'tue' => 'tis', 'wed' => 'ons',
-    'thu' => 'tors', 'fri' => 'fre', 'sat' => 'lör',
+    'thu' => 'tors', 'fri' => 'fre', 'sat' => 'lör'
   }
 
   ::ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
@@ -25,10 +36,10 @@ module SwedishRails
      :short  => "%d %b %H:%M",
      :long   => "%d %B, %Y %H:%M")
 
-  ::Date::MONTHNAMES = [nil] + %w(Januari Februari Mars April Maj Juni Juli Augusti September Oktober November December)
-  ::Date::DAYNAMES = %w(Söndag Måndag Tisdag Onsdag Torsdag Fredag Lördag)
-  ::Date::ABBR_MONTHNAMES = [nil] + %w(Jan Feb Mar Apr Maj Jun Jul Aug Sep Okt Nov Dec)
-  ::Date::ABBR_DAYNAMES = %w(Sön Mån Tis Ons Tor Fre Lör)
+  ::Date::MONTHNAMES = [nil] + %w(januari februari mars april maj juni juli augusti september oktober november december)
+  ::Date::DAYNAMES = %w(söndag måndag tisdag onsdag torsdag fredag lördag)
+  ::Date::ABBR_MONTHNAMES = [nil] + %w(jan feb mar apr maj jun jul aug sep okt nov dec)
+  ::Date::ABBR_DAYNAMES = %w(sön mån tis ons tor fre lör)
 
   class ::Time
     alias :_strftime :strftime
