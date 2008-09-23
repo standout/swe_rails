7f2663ed8580dadb5af0f939963202f03b605866
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2008-09-23 02:25:35 +0200

    Added translations for more validation error messages

--- lib/sv/active_record.rb
+++ lib/sv/active_record.rb
@@ -1,5 +1,6 @@
 module SwedishRails
-  ::ActiveRecord::Errors.default_error_messages.merge!(
+  # Based on activerecord-2.0.2/lib/active_record/validations.rb
+  ::ActiveRecord::Errors.default_error_messages.merge!({
       :inclusion => "finns inte i listan",
       :exclusion => "är reserverat",
       :invalid => "är ogiltigt",
@@ -11,5 +12,13 @@ module SwedishRails
       :too_short => "är för kort (minimum är %d tecken)",
       :wrong_length => "har fel längd (ska vara %d tecken)",
       :taken => "har redan tagits",
-      :not_a_number => "är ej ett nummer")
+      :not_a_number => "är ej ett nummer",
+      :greater_than => "måste vara större än %d",
+      :greater_than_or_equal_to => "måste vara större än eller lika med %d",
+      :equal_to => "måste vara samma som",
+      :less_than => "måste vara mindre än %d",
+      :less_than_or_equal_to => "måste vara mindre än eller lika med %d",
+      :odd => "måste vara udda",
+      :even => "måste vara jämnt"
+  })
 end
